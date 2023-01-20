b227c89a8f05ff457d072e1d75937bcf9f9ccd30
Marine <marine.magnin73@gmail.com>
2023-01-20 16:58:57 +0100

    Fonctionnalités : création de la fonction de recherche simple => la saisie est recherchée dans le titre, la liste des ingrédients et la description

--- public/scripts/utils/filterSearch.js
+++ public/scripts/utils/filterSearch.js
@@ -3,7 +3,61 @@ const searchBtn = document.querySelector("#search-addon");
 
 searchBtn.addEventListener("click", filterSearch);
 
-function filterSearch(){
-    nbCaracteres = searchBtn.length;
-    console.log(nbCaracteres);
+function filterSearch() {
+    // Je récupère la valeur saisie dans l'input et je le mets en minuscules
+   valueSearch = searchForm.value.toLowerCase();
+   console.log(valueSearch);
+   // Je calcule la longueur de la valeur saisie
+   nbCaracteres = valueSearch.length;
+   console.log(nbCaracteres);
+
+   if (nbCaracteres >= 3) {
+      filterRecipes(valueSearch);
+   } else if (nbCaracteres < 3) {
+      console.log("Veuillez entrer plus de 3 caractères");
+   }
 }
+
+function filterRecipes(valueSearch) {
+   const wordToFind = valueSearch;
+   const arrayRecipesLength = recipes.length;
+   console.log("Longueur avant :", arrayRecipesLength);
+   const arrayFilterRecipes = recipes.filter((recipe) => {
+      // Je teste chaque recette
+
+      return (
+            //  if ( recipe.name.includes(wordToFind) || recipe.ingredients.some((ingredient) => ingredient.ingredient.includes(wordToFind)) || recipe.description.includes(wordToFind)) {
+            //     console.log("c'est true")
+            //     return true
+            //  } else {
+            //     console.log("c'est false")
+            //      return false
+            //  }
+         //     );
+
+         //Je recherche le mot dans le titre
+         recipe.name.includes(wordToFind) ||
+         console.log(recipe.name.includes(wordToFind)) ||
+         // Je recherche le mot dans les ingrédients
+         recipe.ingredients.some((ingredient) => ingredient.ingredient.includes(wordToFind)) ||
+         console.log(recipe.ingredients.some((ingredient) => ingredient.ingredient.includes(wordToFind))) ||
+         // Je recherche le mot dans la description
+         recipe.description.includes(wordToFind) ||
+         console.log(recipe.description.includes(wordToFind))
+      );
+   });
+
+   console.log("Longueur après :", arrayFilterRecipes.length);
+
+   if (arrayFilterRecipes.length === 0) {
+      // Message d'erreur
+      console.log('Aucune recette ne correspond à votre critère...Vous pouvez chercher "tarte aux pommes", "poisson", etc.');
+   } else {
+      //J'affiche le résultat dans la console
+      console.log(arrayFilterRecipes);
+   }
+}
+
+// function filterIngredients(){};
+
+// function filterDescription(){};
