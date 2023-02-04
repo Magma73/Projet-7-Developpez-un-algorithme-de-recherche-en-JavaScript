a4068cf7594c4bf0d7a66a9a7e3b38281e609adb
Marine <marine.magnin73@gmail.com>
2023-02-04 19:19:38 +0100

    Structure et style : ajout du message d'erreur lors de la saisie dans input ingrédients

--- public/scripts/utils/filterSearchIngredients.js
+++ public/scripts/utils/filterSearchIngredients.js
@@ -3,7 +3,7 @@ const searchInput = document.querySelector(".button__hidden--ingredient");
 // J'appelle la fonction filterSearch à la saisie dans l'input ingrédient
 searchInput.addEventListener("input", function () {
    const searchValue = this.value;
-   console.log(searchValue);
+//    console.log(searchValue);
    filterRecipes(searchValue);
 
    // Je supprime les éléments de la liste
@@ -33,25 +33,61 @@ function filterRecipes(searchValue) {
          }
       }
    }
-   console.log(arrayFilterRecipes);
-   console.log(arrayFilterRecipes.length);
+//    console.log(arrayFilterRecipes);
+//    console.log(arrayFilterRecipes.length);
 
+   // J'appelle la fonction de création des recettes, des boutons ingrédients, appareils et ustensiles
    displayData(arrayFilterRecipes);
    displayButtonIngredient(arrayFilterRecipes);
    displayButtonAppliance(arrayFilterRecipes);
    displayButtonUstensil(arrayFilterRecipes);
 
+   const wrapperHidden = document.querySelector(".wrapper__hidden--ingredient");
+
    if (arrayFilterRecipes.length === 0) {
     // J'affiche le message d'erreur
-    const wrapperHidden = document.querySelector(".wrapper__hidden--ingredient");
-    // searchInput.setAttribute("placeholder", 'Aucune ingrédient ne correspond à votre critère.');
-    wrapperHidden.setAttribute("data-error", 'Aucune ingrédient ne correspond à votre critère');
+    wrapperHidden.setAttribute("data-error", 'Aucun ingrédient ne correspond à votre critère');
     wrapperHidden.setAttribute("data-error-visible", "true");
-    console.log("Aucune ingrédient ne correspond à votre critère");
+    console.log("Aucun ingrédient ne correspond à votre critère");
  } else {
     //Je n'affiche pas le message d'erreur
     wrapperHidden.setAttribute("data-error", "");
     wrapperHidden.setAttribute("data-error-visible", "");
     console.log("c'est ok");
  }
+
+
+
+//     const ingredients = document.querySelectorAll(".wrapper__option");
+//    ingredients.forEach((btn) =>
+//       btn.addEventListener("click", function (event) {
+//          // Je récupère le texte de l'ingrédient
+//          console.log(this);
+//          const currentIngredient = this.textContent;
+
+//          // Je créé le bouton tag
+//          createTagIngredient(currentIngredient);
+
+//          // Je récupère tous les boutons tag
+//         //  const buttonsTag = document.querySelectorAll(".btn");
+
+//          // si currentIngredient est déjà créé alors je ne recréé pas le tag
+//          //    if(buttonsTag.length !== 0){
+//          //       closeTagIngredient();
+//          //    } else {
+//          //       console.log("Pas de tag");
+//          //    }
+
+//          // Je supprime l'ingrédient cliqué de la liste des ingrédients
+//         //  deleteIngredientList(event);
+
+//          // Pour chaque bouton tag cliqué, je supprime le bouton et je remet l'ingrédient dans la liste
+//     //      buttonsTag.forEach((btn) =>
+//     //      btn.addEventListener("click", deleteButton, reAddIngredientList(event) ));
+//       })
+//    );
+
+
+
+
 }
