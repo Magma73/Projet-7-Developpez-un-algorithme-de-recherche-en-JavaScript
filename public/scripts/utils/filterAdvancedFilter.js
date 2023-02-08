173c98e4c4740297423e83ab3996a58f153cfbb5
Marine <marine.magnin73@gmail.com>
2023-02-08 10:41:15 +0100

    Fonctionnalités : ajout de messages d'erreur pour la recherche avancée => ingrédient, appareil et ustensile

--- public/scripts/utils/filterAdvancedFilter.js
+++ public/scripts/utils/filterAdvancedFilter.js
@@ -8,74 +8,101 @@ let searchValueUstensil = "";
 
 searchInputIngredient.addEventListener("input", function () {
    searchValueIngredient = this.value;
-   /*filterRecipesAdvanced();*/
    filterRecipesAdvanced();
 });
 
 searchInputAppareil.addEventListener("input", function () {
    searchValueAppareil = this.value;
    filterRecipesAdvanced();
-   /*const searchValueAppareil = this.value;*/
 });
 
 searchInputUstensil.addEventListener("input", function () {
    searchValueUstensil = this.value;
    filterRecipesAdvanced();
-   /*const searchValueUstensil = this.value;*/
 });
-
 
 function filterRecipesAdvanced() {
    const ingredientToFind = searchValueIngredient.toLowerCase();
    const appareilToFind = searchValueAppareil.toLowerCase();
    const ustensilToFind = searchValueUstensil.toLowerCase();
 
+   // J'initialise mon tableau arrayAdvancedFilterRecipes
+   let arrayAdvancedFilterRecipes = [];
    console.log(arrayFilterRecipes);
 
-   const arrayAdvancedFilterRecipes = arrayFilterRecipes.filter((recipe) => {
-      // Je teste chaque recette
+   // Je récupère le tableau des recettes filtrées par la recherche simple : arrayFilterRecipes
+   // S'il est égal à 0, je fais mon filtre avancée sur le tableau recipes
+   if (arrayFilterRecipes.length === 0) {
+      arrayAdvancedFilterRecipes = recipes.filter((recipe) => {
+         // Je teste chaque recette
 
-      return (
-         // Je recherche le mot dans les ingrédients
-         recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(ingredientToFind)) &&
-         // Je recherche le mot dans les appareils
-         recipe.appliance.toLowerCase().includes(appareilToFind) &&
-         // Je recherche le mot dans les ustensiles
-         recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(ustensilToFind))
-      );
-   });
+         return (
+            // Je recherche le mot dans les ingrédients
+            recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(ingredientToFind)) &&
+            // Je recherche le mot dans les appareils
+            recipe.appliance.toLowerCase().includes(appareilToFind) &&
+            // Je recherche le mot dans les ustensiles
+            recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(ustensilToFind))
+         );
+      });
 
-   // const arrayConcatIngredientAppareil = arrayFilterAdvancedRecipesIngredients.concat(arrayFilterAdvancedRecipesAppareils);
-   // arrayFilterRecipes = arrayConcatIngredientAppareil.concat(arrayFilterAdvancedRecipesUstensil);
+      // Sinon, je fais mon filtre avancée sur le tableau arrayFilterRecipes
+   } else {
+      arrayAdvancedFilterRecipes = arrayFilterRecipes.filter((recipe) => {
+         // Je teste chaque recette
 
-   //   // Je supprime les doublons
-   //    Array.from(new Set(arrayFilterRecipes));
-
-   //    console.log(arrayFilterAdvancedRecipes.length);
-   //    console.log(arrayFilterAdvancedRecipes);
-
-   console.log(arrayAdvancedFilterRecipes);
-
+         return (
+            // Je recherche le mot dans les ingrédients
+            recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(ingredientToFind)) &&
+            // Je recherche le mot dans les appareils
+            recipe.appliance.toLowerCase().includes(appareilToFind) &&
+            // Je recherche le mot dans les ustensiles
+            recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(ustensilToFind))
+         );
+      });
+   }
    // J'appelle les fonctions de création des recettes, des boutons ingrédients, appareils et ustensiles
    displayDataAdvancedFilter(arrayAdvancedFilterRecipes);
-
    displayListIngredientAdvancedFilter(arrayAdvancedFilterRecipes, searchValueIngredient);
-
    displayListApplianceAdvancedFilter(arrayAdvancedFilterRecipes, searchValueAppareil);
-
    displayListUstensilAdvancedFilter(arrayAdvancedFilterRecipes, searchValueUstensil);
 
+   // J'affiche des messages d'erreur pour chaque ingrédient
+   const wrapperHiddenIngredient = document.querySelector(".wrapper__hidden--ingredient");
 
-      const wrappersHiddens = document.querySelectorAll(".wrapper__hidden");
+   if (arrayAdvancedFilterRecipes.length === 0) {
+      // J'affiche le message d'erreur
+      wrapperHiddenIngredient.setAttribute("data-error", "Aucun ingrédient ne correspond à votre critère");
+      wrapperHiddenIngredient.setAttribute("data-error-visible", "true");
+   } else {
+      //Je n'affiche pas le message d'erreur
+      wrapperHiddenIngredient.setAttribute("data-error", "");
+      wrapperHiddenIngredient.setAttribute("data-error-visible", "");
+   }
 
-      if (arrayFilterRecipes.length === 0) {
-         // J'affiche le message d'erreur
-         wrappersHiddens.setAttribute("data-error", "Aucun ingrédient ne correspond à votre critère");
-         wrappersHiddens.setAttribute("data-error-visible", "true");
-         console.log("Aucun ingrédient ne correspond à votre critère");
-      } else {
-         //Je n'affiche pas le message d'erreur
-         wrappersHiddens.setAttribute("data-error", "");
-         wrappersHiddens.setAttribute("data-error-visible", "");
-      }
+   // J'affiche des messages d'erreur pour chaque appareil
+   const wrappersHiddenAppliance = document.querySelector(".wrapper__hidden--appareil");
+
+   if (arrayAdvancedFilterRecipes.length === 0) {
+      // J'affiche le message d'erreur
+      wrappersHiddenAppliance.setAttribute("data-error", "Aucun appareil ne correspond à votre critère");
+      wrappersHiddenAppliance.setAttribute("data-error-visible", "true");
+   } else {
+      //Je n'affiche pas le message d'erreur
+      wrappersHiddenAppliance.setAttribute("data-error", "");
+      wrappersHiddenAppliance.setAttribute("data-error-visible", "");
+   }
+
+   // J'affiche des messages d'erreur pour chaque ustensile
+   const wrappersHiddenUstensil = document.querySelector(".wrapper__hidden--ustensil");
+
+   if (arrayAdvancedFilterRecipes.length === 0) {
+      // J'affiche le message d'erreur
+      wrappersHiddenUstensil.setAttribute("data-error", "Aucun ustensile ne correspond à votre critère");
+      wrappersHiddenUstensil.setAttribute("data-error-visible", "true");
+   } else {
+      //Je n'affiche pas le message d'erreur
+      wrappersHiddenUstensil.setAttribute("data-error", "");
+      wrappersHiddenUstensil.setAttribute("data-error-visible", "");
+   }
 }
