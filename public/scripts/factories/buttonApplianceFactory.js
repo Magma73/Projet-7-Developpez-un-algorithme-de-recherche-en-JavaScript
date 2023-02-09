0372cf54cf30e67bf32df6762df30b0b60626356
Marine <marine.magnin73@gmail.com>
2023-02-09 21:33:35 +0100

    Vérification du code : suppression du code inutile + mise en forme avec prettier

--- public/scripts/factories/buttonApplianceFactory.js
+++ public/scripts/factories/buttonApplianceFactory.js
@@ -2,17 +2,11 @@ function buttonApplianceFactory(datas) {
    const appliance = datas;
 
    // FACTORY DE LA LISTE DES USTENSILES
-
-   // Initialisation d'un compteur afin de créer les id
-   let currentOption = 0;
-
    function getApplianceWrapperDOM() {
       // Création des éléments wrapper option appareils (li)
       const wrapperOption = document.createElement("li");
       wrapperOption.className = "wrapper__option";
-      currentOption++;
       wrapperOption.textContent = appliance;
-      wrapperOption.setAttribute("id", "ma" + currentOption);
 
       return wrapperOption;
    }
