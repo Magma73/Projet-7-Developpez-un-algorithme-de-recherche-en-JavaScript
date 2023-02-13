3f2838d2255159c58d348355ae310a34192389d6
Marine <marine.magnin73@gmail.com>
2023-02-13 20:53:19 +0100

    Fonctionnalités : refactorisation du code

--- public/scripts/utils/filterTag.js
+++ public/scripts/utils/filterTag.js
@@ -2,72 +2,139 @@ function filterRecipesTags() {
    // Je récupère la valeur saisie dans la recherche simple
    const storedInputValueSearch = localStorage.getItem("valueSearch");
    const valueSearch = JSON.parse(storedInputValueSearch);
-
+   console.log(valueSearch);
+   console.log(valueSearch.length);
+   console.log(typeof valueSearch);
    // Je récupère le tableau storé des recettes filtrées par la recherche avancée
    const storedArrayAdvancedFilterRecipes = localStorage.getItem("arrayAdvancedFilterRecipes");
    const arrayAdvancedFilterRecipesStored = JSON.parse(storedArrayAdvancedFilterRecipes);
 
-   // Si la recherche simple utilisée et qu'au moins un des champs de la recherche avancée utilisé, alors j'utilise le tableau storé : recherche simple + recherche avancée
-   if (valueSearch.length !== 0 && (searchValueIngredient.length !== 0 || searchValueAppareil.length !== 0 || searchValueUstensil.length !== 0)) {
-      arrayAdvancedFilterRecipes = arrayAdvancedFilterRecipesStored.filter((recipe) => {
-         return (
-            selectedTagsIngredients.every((selectedTag) => {
-               return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
-            }) &&
-            selectedTagsAppliance.every((selectedTag) => {
-               return recipe.appliance.toLowerCase().includes(selectedTag);
-            }) &&
-            selectedTagsUstensil.every((selectedTag) => {
-               return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
-            })
-         );
-      });
-      // Si la recherche simple pas utilisée et qu'au moins un des champs de la recherche avancée utilisé, alors j'utilise le tableau storé : recherche avancée
-   } else if (valueSearch.length === 0 && (searchValueIngredient.length !== 0 || searchValueAppareil.length !== 0 || searchValueUstensil.length !== 0)) {
-      arrayAdvancedFilterRecipes = arrayAdvancedFilterRecipesStored.filter((recipe) => {
-         return (
-            selectedTagsIngredients.every((selectedTag) => {
-               return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
-            }) &&
-            selectedTagsAppliance.every((selectedTag) => {
-               return recipe.appliance.toLowerCase().includes(selectedTag);
-            }) &&
-            selectedTagsUstensil.every((selectedTag) => {
-               return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
-            })
-         );
-      });
-      // Si la recherche simple utilisée, alors j'utilise le tableau arrayFilterRecipes : recherche simple utilisée
-   } else if (valueSearch.length !== 0) {
-      arrayAdvancedFilterRecipes = arrayFilterRecipes.filter((recipe) => {
-         return (
-            selectedTagsIngredients.every((selectedTag) => {
-               return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
-            }) &&
-            selectedTagsAppliance.every((selectedTag) => {
-               return recipe.appliance.toLowerCase().includes(selectedTag);
-            }) &&
-            selectedTagsUstensil.every((selectedTag) => {
-               return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
-            })
-         );
-      });
-      // Si la recherche simple pas utilisée, alors j'utilise le tableau recipes : recherche simple pas utilisée
-   } else if (valueSearch.length === 0) {
-      arrayAdvancedFilterRecipes = recipes.filter((recipe) => {
-         return (
-            selectedTagsIngredients.every((selectedTag) => {
-               return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
-            }) &&
-            selectedTagsAppliance.every((selectedTag) => {
-               return recipe.appliance.toLowerCase().includes(selectedTag);
-            }) &&
-            selectedTagsUstensil.every((selectedTag) => {
-               return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
-            })
-         );
-      });
-   }
+   // // Si la recherche simple utilisée et qu'au moins un des champs de la recherche avancée utilisé, alors j'utilise le tableau storé : recherche simple + recherche avancée
+   // if (valueSearch.length !== 0 && (searchValueIngredient.length !== 0 || searchValueAppareil.length !== 0 || searchValueUstensil.length !== 0)) {
+   //    arrayAdvancedFilterRecipes = arrayAdvancedFilterRecipesStored.filter((recipe) => {
+   //       return (
+   //          selectedTagsIngredients.every((selectedTag) => {
+   //             return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
+   //          }) &&
+   //          selectedTagsAppliance.every((selectedTag) => {
+   //             return recipe.appliance.toLowerCase().includes(selectedTag);
+   //          }) &&
+   //          selectedTagsUstensil.every((selectedTag) => {
+   //             return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
+   //          })
+   //       );
+   //    });
+   //    // Si la recherche simple pas utilisée et qu'au moins un des champs de la recherche avancée utilisé, alors j'utilise le tableau storé : recherche avancée
+   // } else if (valueSearch.length === 0 && (searchValueIngredient.length !== 0 || searchValueAppareil.length !== 0 || searchValueUstensil.length !== 0)) {
+   //    arrayAdvancedFilterRecipes = arrayAdvancedFilterRecipesStored.filter((recipe) => {
+   //       return (
+   //          selectedTagsIngredients.every((selectedTag) => {
+   //             return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
+   //          }) &&
+   //          selectedTagsAppliance.every((selectedTag) => {
+   //             return recipe.appliance.toLowerCase().includes(selectedTag);
+   //          }) &&
+   //          selectedTagsUstensil.every((selectedTag) => {
+   //             return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
+   //          })
+   //       );
+   //    });
+   //    // Si la recherche simple utilisée, alors j'utilise le tableau arrayFilterRecipes : recherche simple utilisée
+   // } else if (valueSearch.length !== 0) {
+   //    arrayAdvancedFilterRecipes = arrayFilterRecipes.filter((recipe) => {
+   //       return (
+   //          selectedTagsIngredients.every((selectedTag) => {
+   //             return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
+   //          }) &&
+   //          selectedTagsAppliance.every((selectedTag) => {
+   //             return recipe.appliance.toLowerCase().includes(selectedTag);
+   //          }) &&
+   //          selectedTagsUstensil.every((selectedTag) => {
+   //             return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
+   //          })
+   //       );
+   //    });
+   //    // Si la recherche simple pas utilisée, alors j'utilise le tableau recipes : recherche simple pas utilisée
+   // } else if (valueSearch.length === 0) {
+   //    arrayAdvancedFilterRecipes = recipes.filter((recipe) => {
+   //       return (
+   //          selectedTagsIngredients.every((selectedTag) => {
+   //             return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
+   //          }) &&
+   //          selectedTagsAppliance.every((selectedTag) => {
+   //             return recipe.appliance.toLowerCase().includes(selectedTag);
+   //          }) &&
+   //          selectedTagsUstensil.every((selectedTag) => {
+   //             return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
+   //          })
+   //       );
+   //    });
+   // }
+
+
+      // Si la recherche simple utilisée et qu'au moins un des champs de la recherche avancée utilisé, alors j'utilise le tableau storé : recherche simple + recherche avancée
+      if (valueSearch.trim().length !== 0 && (searchValueIngredient.trim().length !== 0 || searchValueAppareil.trim().length !== 0  || searchValueUstensil.trim().length !== 0 )) {
+         arrayAdvancedFilterRecipes = arrayAdvancedFilterRecipesStored.filter((recipe) => {
+            return (
+               selectedTagsIngredients.every((selectedTag) => {
+                  return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
+               }) &&
+               selectedTagsAppliance.every((selectedTag) => {
+                  return recipe.appliance.toLowerCase().includes(selectedTag);
+               }) &&
+               selectedTagsUstensil.every((selectedTag) => {
+                  return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
+               })
+            );
+         });
+         // Si la recherche simple pas utilisée et qu'au moins un des champs de la recherche avancée utilisé, alors j'utilise le tableau storé : recherche avancée
+      } else if (valueSearch.trim().length === 0  && (searchValueIngredient.trim().length !== 0 || searchValueAppareil.trim().length !== 0 || searchValueUstensil.trim().length !== 0)) {
+         arrayAdvancedFilterRecipes = arrayAdvancedFilterRecipesStored.filter((recipe) => {
+            return (
+               selectedTagsIngredients.every((selectedTag) => {
+                  return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
+               }) &&
+               selectedTagsAppliance.every((selectedTag) => {
+                  return recipe.appliance.toLowerCase().includes(selectedTag);
+               }) &&
+               selectedTagsUstensil.every((selectedTag) => {
+                  return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
+               })
+            );
+         });
+         // Si la recherche simple utilisée, alors j'utilise le tableau arrayFilterRecipes : recherche simple utilisée
+      } else if (valueSearch.trim().length !== 0) {
+         arrayAdvancedFilterRecipes = arrayFilterRecipes.filter((recipe) => {
+            return (
+               selectedTagsIngredients.every((selectedTag) => {
+                  return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
+               }) &&
+               selectedTagsAppliance.every((selectedTag) => {
+                  return recipe.appliance.toLowerCase().includes(selectedTag);
+               }) &&
+               selectedTagsUstensil.every((selectedTag) => {
+                  return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
+               })
+            );
+         });
+         // Si la recherche simple pas utilisée, alors j'utilise le tableau recipes : recherche simple pas utilisée
+      } else if (valueSearch.trim().length === 0) {
+         arrayAdvancedFilterRecipes = recipes.filter((recipe) => {
+            return (
+               selectedTagsIngredients.every((selectedTag) => {
+                  return recipe.ingredients.some((ingredient) => ingredient.ingredient.toLowerCase().includes(selectedTag));
+               }) &&
+               selectedTagsAppliance.every((selectedTag) => {
+                  return recipe.appliance.toLowerCase().includes(selectedTag);
+               }) &&
+               selectedTagsUstensil.every((selectedTag) => {
+                  return recipe.ustensils.some((ustensil) => ustensil.toLowerCase().includes(selectedTag));
+               })
+            );
+         });
+      }
+
+
    // J'appelle les fonctions de création des recettes, des boutons ingrédients, appareils et ustensiles
    displayDataTagFilter(arrayAdvancedFilterRecipes);
    displayListIngredientTagFilter(arrayAdvancedFilterRecipes, selectedTagsIngredients);
